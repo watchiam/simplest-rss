b1c1ea174eea0935cf9d8c8e906084cebb654d68
watchiam <watchiam@gmail.com>
2017-10-03 15:50:14 -0500

    First Commit
    Electron Quick Start base code

---
+++ app.js
@@ -0,0 +1,9 @@
+$.get('https://stackoverflow.com/feeds/question/10943544', function(data) {
+	$(data).find("entry").each(function(){// or item
+		var el = $this;
+		console.log("-----------");
+		console.log("title		:	" + el.find("title").text());
+		console.log("author		:	" + el.find("author").text());
+		console.log("description:	" + el.find("description").text());
+	});
+});
